a895f1de277e947235de515b11b299efe7f77e06
User <User@LAPTOP-8T5Q7A53>
2018-10-23 14:01:19 +0900

    aaaa

--- src/main/java/com/kfi/ysy/admin/controller/AdminController.java
+++ src/main/java/com/kfi/ysy/admin/controller/AdminController.java
@@ -1,5 +1,16 @@
 package com.kfi.ysy.admin.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+
+@Controller
 public class AdminController {
 
+	@RequestMapping(value = "/admin", method = RequestMethod.GET)
+	public String main() {
+		return ".admin.main";
+	}
 }
