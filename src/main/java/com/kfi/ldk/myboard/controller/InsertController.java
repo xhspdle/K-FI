7cc13dc71bf8f7bdfd3c116c9f790f9336bdfac7
DG <DG@local>
2018-10-24 11:08:48 +0900

    [돈규] 컨트롤러 벨류속성 추가

--- src/main/java/com/kfi/ldk/myboard/controller/InsertController.java
+++ src/main/java/com/kfi/ldk/myboard/controller/InsertController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.kfi.ldk.service.MyBoardService;
 import com.kfi.ldk.vo.MyBoardVo;
 
-@Controller
+@Controller(value="MyBoardInsertController")
 public class InsertController {
 	@Autowired private MyBoardService service;
 	@RequestMapping(value="/mypage/myboard/insert",method=RequestMethod.GET)
