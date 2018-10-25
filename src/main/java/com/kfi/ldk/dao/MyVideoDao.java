2246a0d4c760e54af6ded9a300fe8c2c1754c0ae
LDK <xhspdle@naver.com>
2018-10-25 17:26:16 +0900

    [동광] 마이바티스 꼬인거 정리

---
+++ src/main/java/com/kfi/ldk/dao/MyVideoDao.java
@@ -0,0 +1,36 @@
+package com.kfi.ldk.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.kfi.ldk.vo.MyVideoVo;
+
+@Repository
+public class MyVideoDao {
+	@Autowired private SqlSession session;
+	private static final String NAMESPACE="com.kfi.mybatis.ldk.MyVideoMapper";
+	public int getMaxNum() {
+		return session.selectOne(NAMESPACE + ".getMaxNum");
+	}
+	public int getCount() {
+		return session.selectOne(NAMESPACE + ".getCount");
+	}
+	public int insert(MyVideoVo vo) {
+		return session.insert(NAMESPACE + ".insert", vo);
+	}
+	public int update(MyVideoVo vo) {
+		return session.update(NAMESPACE + ".update", vo);
+	}
+	public int delete(int mv_num) {
+		return session.delete(NAMESPACE + ".delete", mv_num);
+	}
+	public List<MyVideoVo> list() {
+		return session.selectList(NAMESPACE + ".list");
+	}
+	public MyVideoVo getinfo(int mv_num) {
+		return session.selectOne(NAMESPACE + ".getinfo", mv_num);
+	}
+}
