13d2a96e9210d5d217598375bb9a6fb2fa33ea59
JHTA <JHTA@JHTA-PC>
2018-11-09 15:04:31 +0900

    [성윤] 파일 확인 및 중간확인

--- src/main/java/com/kfi/ysy/admin/controller/UserBoardListView.java
+++ src/main/java/com/kfi/ysy/admin/controller/UserBoardListView.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller(value="Admin_UserBoardListView")
-public class AdBoardListView {
-	@RequestMapping(value="boardlistview",method=RequestMethod.GET)
+public class UserBoardListView {
+	@RequestMapping(value="userboardlistview",method=RequestMethod.GET)
 	public String boardlistview() {
 		
 		
-		return ".admin.boardlistview";
+		return ".admin.ublistview";
 	}
 }
--- src/main/webapp/WEB-INF/views/admin/main.jsp
+++ src/main/webapp/WEB-INF/views/admin/main.jsp
@@ -54,7 +54,7 @@
 					<ul class="nav navbar-nav navbar-right">
 						<li><a href="mblist">회원관리</a></li>
 						<li><a href="aclist">신고관리</a></li>
-						<li><a href="boardlistview">통계관리</a></li>
+						<li><a href="userboardlistview">통계관리</a></li>
 						<li><a href="ablist">공지사항</a></li>
 						<li><a href="faqlist">Q&#38;A</a></li>
 					</ul>
