eb7aa42a65b0ac21718d069cbbbdadcb2196e088
User <User@LAPTOP-8T5Q7A53>
2018-11-21 22:48:01 +0900

    [성윤] 중간확인

--- src/main/java/com/kfi/ysy/service/CommRefuseServiceImpl.java
+++ src/main/java/com/kfi/ysy/service/CommRefuseServiceImpl.java
@@ -1,13 +1,19 @@
 package com.kfi.ysy.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kfi.jyi.dao.CommRefuseDao;
+import com.kfi.jyi.vo.CommRefuseVo;
 import com.kfi.ldk.service.CommonService;
 
+@Service
 public class CommRefuseServiceImpl implements CommonService {
-
+	@Autowired
+	private CommRefuseDao dao;
 	@Override
 	public int getMaxNum() {
-		// TODO Auto-generated method stub
-		return 0;
+		return dao.getMaxNum();
 	}
 
 	@Override
@@ -18,8 +24,14 @@ public class CommRefuseServiceImpl implements CommonService {
 
 	@Override
 	public int insert(Object data) {
-		// TODO Auto-generated method stub
-		return 0;
+		CommRefuseVo vo=(CommRefuseVo)data; 
+		int cr_num=getMaxNum()+1;
+		vo.setCr_num(cr_num);
+		int result=dao.insert(vo);
+		System.out.println(cr_num);
+		System.out.println(result);
+		
+		return dao.insert(vo);
 	}
 
 	@Override
